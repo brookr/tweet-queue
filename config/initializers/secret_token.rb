b483a2413fa1c10c876c7d21f02bcbfeaac2fd5b
Brook Riggio <brooke.riggio@gmail.com>
2013-09-11 15:52:29 -0700

    Start of an example Rails app, to show how to make a Tweet Queue.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TweetQueue::Application.config.secret_key_base = 'ca08e0b110269d723e50734e125c34ff8ecece9177cef4145e73707d38a4bfe119479e1667669a47d4738c8265f2dc354e1bb26cb448c832ebf758a44b268704'
